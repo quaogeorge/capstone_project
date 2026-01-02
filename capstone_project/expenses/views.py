d485c7cf4d6d97f9c9eb303f03c2349e1a07d8f2
Kojo Quao <quaogeorge@icloud.com>
2026-01-02 16:01:41 +0000

    progress

--- capstone_project/expenses/views.py
+++ capstone_project/expenses/views.py
@@ -1,29 +1,21 @@
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework import viewsets
-from .models import Expense
-from .serializers import ExpenseSerializer
-from django.db.models import Sum
+from rest_framework import generics, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Sum
+from django.db.models.functions import TruncWeek, TruncMonth
+
+from .models import Expense
+from .serializers import ExpenseSerializer
 
 
 class ExpenseListCreateView(generics.ListCreateAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
+
 class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-
-class ExpenseViewSet(viewsets.ModelViewSet):
-    serializer_class = ExpenseSerializer
-
-    def get_queryset(self):
-        return Expense.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
 
 class ExpenseViewSet(viewsets.ModelViewSet):
@@ -43,13 +35,43 @@ class ExpenseViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(date__range=[start_date, end_date])
 
         return queryset
-    
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     @action(detail=False, methods=['get'])
     def summary(self, request):
-        expenses = Expense.objects.filter(user=request.user)
+        total = (
+            Expense.objects
+            .filter(user=request.user)
+            .aggregate(total_spent=Sum('amount'))['total_spent']
+            or 0
+        )
 
-        total = expenses.aggregate(total_spent=Sum('amount'))['total_spent'] or 0
+        return Response({'total_spent': total})
 
-        return Response({
-        'total_spent': total
-    })
+    @action(detail=False, methods=['get'])
+    def weekly_summary(self, request):
+        expenses = (
+            Expense.objects
+            .filter(user=request.user)
+            .annotate(week=TruncWeek('date'))
+            .values('week')
+            .annotate(total=Sum('amount'))
+            .order_by('week')
+        )
+
+        return Response(expenses)
+
+    @action(detail=False, methods=['get'])
+    def monthly_summary(self, request):
+        expenses = (
+            Expense.objects
+            .filter(user=request.user)
+            .annotate(month=TruncMonth('date'))
+            .values('month')
+            .annotate(total=Sum('amount'))
+            .order_by('month')
+        )
+
+        return Response(expenses)
